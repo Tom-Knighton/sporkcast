6a5881e1e77134e64f4138f49d488811cf88edce
Tom K <tomknighton@icloud.com>
2025-08-22 23:10:19 +0100

    Initial Commit

---
+++ sporkcast/sporkcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  sporkcastApp.swift
+//  sporkcast
+//
+//  Created by Tom Knighton on 22/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct sporkcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
